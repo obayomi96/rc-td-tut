63f0cda7c9aaa63ae5a39c5fc56431962a302129
Martins Obayomi <obayomimartins96@gmail.com>
2021-11-11 11:44:21 +0100

    Update search with usePaginatedQuery hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import React, { useState } from 'react';
+import { usePaginatedQuery } from 'react-query';
 import ImageCard from './components/ImageCard';
 import ImageSearch from './components/ImageSearch';
 
-const fetchData = async () => {
-  const response = await fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&image_type=photo&pretty=true`);
+const fetchData = async (key, term) => {
+  const response = await fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`);
   return response.json();
 }
 
 function App() {
-
-  const { data, status } = useQuery('images', fetchData);
+  const [term, setTerm] = useState('');
+  const { 
+    resolvedData, 
+    status 
+  } = usePaginatedQuery(['images', term], fetchData);
 
   return (
     <div className="container mx-auto">
-      <ImageSearch />
+      <ImageSearch searchText={(text) => setTerm(text)} />
 
-      {status !== 'loading' && data.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+      {status !== 'loading' && resolvedData.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
 
       {status === 'loading' ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
-        {data.hits.map(image => (
+        {resolvedData.hits.map(image => (
           <ImageCard key={image.id} image={image} />
         ))}
       </div>}
